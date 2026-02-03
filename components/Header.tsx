e51e1672bb0acdf667cc5af775d1092796b29fa4
Razaan <razaanreza0705@gmail.com>
2026-02-03 18:48:03 +0600

    css updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,11 +11,11 @@ function Header() {
       <div className="container header-wrapper">
         <Link href="/">
           <Image
-            src="/assets/icons/logo.svg"
-            alt="Signalist logo"
+            src="/assets/icons/zarionLogo.png"
+            alt="Zarion logo"
             width={140}
             height={32}
-            className="h-8 w-auto cursor-pointer"
+            className="h-8 w-auto scale-200 pl-2 cursor-pointer"
           />
         </Link>
         <nav className="hidden sm:block">
